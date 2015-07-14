810d249c1d652123d7a03bc75008dd0faeaf8438
Tom Herold <tom.herold@scalableminds.com>
2015-07-14 14:20:22 +0200

    included apple favicon in build process

--- client/scripts/main.jsx
+++ client/scripts/main.jsx
@@ -3,6 +3,7 @@ import Router from "./lib/router";
 
 import FavIcon from "../images/favicon-96x96.png";
 import FavIcon2 from "../images/favicon.ico";
+import FavIcon3 from "../images/apple-touch-icon-114x114.png";
 
 import Styles from "../styles/main.less";
 import MaterializeCSS from "materialize-css";
