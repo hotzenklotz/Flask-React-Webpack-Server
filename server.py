dbb6036e36a50d243ad6849c20acb4c0f398cb87
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-07-23 12:50:20 +0200

    code cleanup

--- server.py
+++ server.py
@@ -2,7 +2,6 @@
 import subprocess
 import time
 from os import path
-import os           
 import shutil
 
 import numpy as np
@@ -13,13 +12,16 @@ from flask_extensions import *
 import math
 
 # Local predicition modules
-# sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'prediction')) #find modules in parent_folder/predictions
+# find modules in parent_folder/predictions
+# sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'prediction')) 
 
 static_assets_path = path.join(path.dirname(__file__), "dist")
 app = Flask(__name__, static_folder=static_assets_path)
 CORS(app)
 
+
 LABEL_MAPPING = {}
+
 
 def load_label_mapping():
     global LABEL_MAPPING
@@ -29,43 +31,56 @@ def load_label_mapping():
             LABEL_MAPPING[int(splitted[1])] = splitted[0]
         
         
-def slice_in_chunks(input, n):
-    for i in range(0, len(input), n):
-        yield input[i:i+n]
+def slice_in_chunks(els, n):
+    for i in range(0, len(els), n):
+        yield els[i:i+n]
 
-        # ----- Video -----------
+
+# ----- Video -----------
 import caffe
+
+
 def get_frames(video_file_name, frame_rate, output_folder):
     cmd = "ffmpeg -n -nostdin -i \"%s\" -r \"%d\" -qscale:v 2 \"%s/%%4d.jpg\"" % (video_file_name, frame_rate, output_folder)
     subprocess.call(cmd, shell=True)
     return output_folder
 
+
 def load_frame_data(frame_file):
     return caffe.io.load_image(frame_file)
 
+
+def load_frames(frame_list, w, h, transformer):
+    data = np.zeros((len(frame_list), 3, w, h))
+
+    for idx, frame in enumerate(frame_list):
+        data[idx, :, :, :] = transformer.preprocess('data', load_frame_data(frame))
+    
+    return data
+
+
 def frames_in_folder(folder):
     return map(lambda p: os.path.join(folder, p), os.listdir(folder))
+
 
 def predict_caffe(frame_files):
     net = caffe.Net(app.config["CAFFE_SPATIAL_PROTO"], app.config["CAFFE_SPATIAL_MODEL"], caffe.TEST)
     batch_size = app.config["CAFFE_BATCH_LIMIT"]
 
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-    transformer.set_transpose('data', (2,0,1))
+    transformer.set_transpose('data', (2, 0, 1))
     transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
-    transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
-    transformer.set_mean('data', np.load(app.config["CAFFE_SPATIAL_MEAN"]).mean(1).mean(1)) # mean pixel
+    transformer.set_channel_swap('data', (2, 1, 0))  # the reference model has channels in BGR order instead of RGB
+    transformer.set_mean('data', np.load(app.config["CAFFE_SPATIAL_MEAN"]).mean(1).mean(1))  # mean pixel
 
     caffe.set_mode_cpu()
     
     results = np.zeros((len(frame_files), app.config["CAFFE_NUM_LABELS"]))
     
     for cidx, chunk in enumerate(slice_in_chunks(frame_files, batch_size)):
-        data = np.zeros((len(chunk), 3, net.blobs['data'].data.shape[2], net.blobs['data'].data.shape[3]))
         
-        for idx, frame in enumerate(chunk):
-            data[idx, :, :, :] = transformer.preprocess('data', load_frame_data(frame))
-
+        data = load_frames(chunk, net.blobs['data'].data.shape[2], net.blobs['data'].data.shape[3], transformer)
+        
         net.blobs['data'].reshape(*data.shape)
     
         out = net.forward_all(data=data)
@@ -73,8 +88,8 @@ def predict_caffe(frame_files):
         results[cidx*batch_size: cidx*batch_size+len(chunk), :] = out['prob']
     return results
 
-# ----- Routes ----------
 
+# ----- Routes ----------
 @app.route("/", defaults={"fall_through": ""})
 @app.route("/<path:fall_through>")
 def index(fall_through):
@@ -95,16 +110,16 @@ def send_video(video_path):
 
 
 @app.route("/api/upload", methods=["POST"])
-def uploadVideo():
-    def is_allowed(filename):
-        return len(filter(lambda ext: ext in filename, ["avi", "mpg", "mpeg", "mkv", "webm", "mp4", "mov"])) > 0
+def upload_video():
+    def is_allowed(file_name):
+        return len(filter(lambda ext: ext in file_name, ["avi", "mpg", "mpeg", "mkv", "webm", "mp4", "mov"])) > 0
 
-    file = request.files.getlist("video")[0]
+    video_file = request.files.getlist("video")[0]
 
-    if file and is_allowed(file.filename):
-        filename = secure_filename(file.filename)
-        file_path = path.join(app.config["UPLOAD_FOLDER"], filename)
-        file.save(file_path)
+    if file and is_allowed(video_file.filename):
+        file_name = secure_filename(video_file.filename)
+        file_path = path.join(app.config["UPLOAD_FOLDER"], file_name)
+        video_file.save(file_path)
 
         response = jsonify(get_prediction(file_path))
     else:
@@ -138,13 +153,13 @@ def get_prediction(file_path):
     os.makedirs(temp_dir)
     get_frames(file_path, 25, temp_dir)
 
-    #  predictions = external_script.predict(file_path)
+    # predictions = external_script.predict(file_path)
     predictions = predict_caffe(frames_in_folder(temp_dir))
     print "Shape of predicitons", predictions.shape, "Type", type(predictions)
     
     print "Max ", np.argmax(predictions, axis=1)
 
-    file_path = file_path + "?cachebuster=%s" % time.time()
+    file_path += "?cachebuster=%s" % time.time()
     result = {
         "video": {
             "url": "%s" % file_path,
@@ -153,17 +168,17 @@ def get_prediction(file_path):
         "frames": []
     }
 
-    for index, row in enumerate(predictions):
+    for idx, row in enumerate(predictions):
 
-        pred_per_label = []
+        predictions_per_label = []
 
         five_best = np.argpartition(row, -5)[-5:]
         for i in five_best:
-            pred_per_label.append({"label": LABEL_MAPPING[i], "prob": row[i].item()})
+            predictions_per_label.append({"label": LABEL_MAPPING[i], "prob": row[i].item()})
 
         new_frame = {
-            "frameNumber": index,
-            "predictions": pred_per_label
+            "frameNumber": idx,
+            "predictions": predictions_per_label
         }
 
         result["frames"].append(new_frame)
@@ -185,7 +200,7 @@ if __name__ == "__main__":
         CAFFE_NUM_LABELS=101,
         CAFFE_SPATIAL_PROTO="/Users/tombocklisch/Documents/Studium/Master Project/models/deploy.prototxt",
         CAFFE_SPATIAL_MODEL="/Users/tombocklisch/Documents/Studium/Master Project/models/_iter_70000.caffemodel",
-        CAFFE_SPATIAL_MEAN="/Users/tombocklisch/Documents/Studium/Master Project/models/ilsvrc_2012_mean.npy" #"/home/mpss2015/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy"
+        CAFFE_SPATIAL_MEAN="/Users/tombocklisch/Documents/Studium/Master Project/models/ilsvrc_2012_mean.npy"  #"/home/mpss2015/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy"
     )
 
     load_label_mapping()
