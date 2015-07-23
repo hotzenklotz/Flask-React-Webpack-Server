efd8e2527197299eede13dcf05ea88efd04c716d
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-07-23 10:30:44 +0200

    working on integrating correct labels

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from flask.ext.cors import CORS
 from flask import *
 from werkzeug import secure_filename
 from flask_extensions import *
+import math
 
 # Local predicition modules
 # sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'prediction')) #find modules in parent_folder/predictions
@@ -17,43 +18,60 @@ from flask_extensions import *
 static_assets_path = path.join(path.dirname(__file__), "dist")
 app = Flask(__name__, static_folder=static_assets_path)
 CORS(app)
-LABELS = ["MilitaryParade", "PlayingSitar", "TennisSwing", "VolleyballSpiking", "HandstandPushups", "Typing", "TableTennisShot", "BrushingTeeth", "StillRings", "HulaHoop", "PlayingDaf", "Drumming", "CricketShot", "ApplyLipstick", "WritingOnBoard", "TrampolineJumping", "YoYo", "BoxingPunchingBag", "Knitting", "Basketball", "PlayingFlute", "JavelinThrow", "ApplyEyeMakeup", "HeadMassage", "RopeClimbing", "WalkingWithDog", "Skijet", "PlayingTabla", "HighJump", "CleanAndJerk", "Fencing", "Archery", "UnevenBars", "HammerThrow", "BaseballPitch", "Haircut", "SalsaSpin", "FrontCrawl", "BabyCrawling", "CuttingInKitchen", "SoccerJuggling", "Billiards", "SkyDiving", "HandstandWalking", "PlayingCello", "Surfing", "SkateBoarding", "Mixing", "FloorGymnastics", "SoccerPenalty", "Swing", "CliffDiving", "Skiing", "Lunges", "ShavingBeard", "BasketballDunk", "HorseRace", "BodyWeightSquats", "Rowing", "RockClimbingIndoor", "HorseRiding", "BlowDryHair", "Diving", "FieldHockeyPenalty", "JumpingJack", "PlayingPiano", "BreastStroke", "JugglingBalls", "FrisbeeCatch", "Rafting", "Biking", "BoxingSpeedBag", "MoppingFloor", "CricketBowling", "ThrowDiscus", "LongJump", "TaiChi", "PushUps", "BandMarching", "JumpRope", "PullUps", "SumoWrestling", "PlayingViolin", "Nunchucks", "Hammering", "GolfSwing", "PommelHorse", "PoleVault", "PizzaTossing", "PlayingDhol", "Punch", "ParallelBars", "PlayingGuitar", "Kayaking", "BalanceBeam", "WallPushups", "IceDancing", "BenchPress", "BlowingCandles", "Bowling", "Shotput"]
 
-# ----- Video -----------
+LABEL_MAPPING = {}
+
+def load_label_mapping():
+    global LABEL_MAPPING
+    with open(app.config["LABEL_MAPPING"], "r") as f:
+        for line in f:
+            splitted = line.split(" ")
+            LABEL_MAPPING[int(splitted[1])] = splitted[0]
+        
+        
+def slice_in_chunks(input, n):
+    for i in range(0, len(input), n):
+        yield input[i:i+n]
+
+        # ----- Video -----------
 import caffe
 def get_frames(video_file_name, frame_rate, output_folder):
-    cmd = "ffmpeg -n -nostdin -i \"%s\" -r \"%d\" -qscale:v 2 \"%s/%%3d.jpg\"" % (video_file_name, frame_rate, output_folder)
+    cmd = "ffmpeg -n -nostdin -i \"%s\" -r \"%d\" -qscale:v 2 \"%s/%%4d.jpg\"" % (video_file_name, frame_rate, output_folder)
     subprocess.call(cmd, shell=True)
     return output_folder
 
-def load_frame_data(folder):
-    frame_files = os.listdir(folder)
-    print frame_files
-    frame_data = []
-    for idx, frame_file in enumerate(frame_files):
-        img = caffe.io.load_image(os.path.join(folder, frame_file))
-        frame_data.append(img)
-    return frame_data
+def load_frame_data(frame_file):
+    return caffe.io.load_image(frame_file)
 
-def predict_caffe(frames):
+def frames_in_folder(folder):
+    return map(lambda p: os.path.join(folder, p), os.listdir(folder))
+
+def predict_caffe(frame_files):
     net = caffe.Net(app.config["CAFFE_SPATIAL_PROTO"], app.config["CAFFE_SPATIAL_MODEL"], caffe.TEST)
+    batch_size = app.config["CAFFE_BATCH_LIMIT"]
 
     transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
     transformer.set_transpose('data', (2,0,1))
     transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
     transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
     transformer.set_mean('data', np.load(app.config["CAFFE_SPATIAL_MEAN"]).mean(1).mean(1)) # mean pixel
-    
-    data = np.zeros((len(frames), 3, net.blobs['data'].data.shape[2], net.blobs['data'].data.shape[3]))
-    
-    for idx, frame in enumerate(frames):
-        data[idx, :, :, :] = transformer.preprocess('data', frame)
-        
-    caffe.set_mode_cpu()
-    net.blobs['data'].reshape(*data.shape)
 
-    out = net.forward_all(data=data)
-    return out['prob']
+    caffe.set_mode_cpu()
+    
+    results = np.zeros((len(frame_files), app.config["CAFFE_NUM_LABELS"]))
+    
+    for cidx, chunk in enumerate(slice_in_chunks(frame_files, batch_size)):
+        data = np.zeros((len(chunk), 3, net.blobs['data'].data.shape[2], net.blobs['data'].data.shape[3]))
+        
+        for idx, frame in enumerate(chunk):
+            data[idx, :, :, :] = transformer.preprocess('data', load_frame_data(frame))
+
+        net.blobs['data'].reshape(*data.shape)
+    
+        out = net.forward_all(data=data)
+        print "Finished chunk %d of %d" % (cidx, math.ceil(len(frame_files) * 1.0 / batch_size))
+        results[cidx*batch_size: cidx*batch_size+len(chunk), :] = out['prob']
+    return results
 
 # ----- Routes ----------
 
@@ -118,13 +136,13 @@ def get_prediction(file_path):
     temp_dir = path.join(app.config["TEMP_FOLDER"], "", file_path)
     shutil.rmtree(temp_dir, ignore_errors=True)
     os.makedirs(temp_dir)
-    get_frames(file_path, 2, temp_dir)
-    frames = load_frame_data(temp_dir)
+    get_frames(file_path, 25, temp_dir)
 
-    
     #  predictions = external_script.predict(file_path)
-    predictions = predict_caffe(frames).astype(np.float32, copy=False)
+    predictions = predict_caffe(frames_in_folder(temp_dir))
     print "Shape of predicitons", predictions.shape, "Type", type(predictions)
+    
+    print "Max ", np.argmax(predictions, axis=1)
 
     file_path = file_path + "?cachebuster=%s" % time.time()
     result = {
@@ -141,7 +159,7 @@ def get_prediction(file_path):
 
         five_best = np.argpartition(row, -5)[-5:]
         for i in five_best:
-            pred_per_label.append({"label": LABELS[i], "prob": row[i].item()})
+            pred_per_label.append({"label": LABEL_MAPPING[i], "prob": row[i].item()})
 
         new_frame = {
             "frameNumber": index,
@@ -150,10 +168,7 @@ def get_prediction(file_path):
 
         result["frames"].append(new_frame)
 
-
     print result
-
-
     return result
 
 
@@ -165,10 +180,15 @@ if __name__ == "__main__":
         CORS_HEADERS="Content-Type",
         UPLOAD_FOLDER="videos",
         TEMP_FOLDER="temp",
+        LABEL_MAPPING="/Users/tombocklisch/Documents/Studium/Master Project/models/label_mapping.txt",
+        CAFFE_BATCH_LIMIT=50,
+        CAFFE_NUM_LABELS=101,
         CAFFE_SPATIAL_PROTO="/Users/tombocklisch/Documents/Studium/Master Project/models/deploy.prototxt",
         CAFFE_SPATIAL_MODEL="/Users/tombocklisch/Documents/Studium/Master Project/models/_iter_70000.caffemodel",
         CAFFE_SPATIAL_MEAN="/Users/tombocklisch/Documents/Studium/Master Project/models/ilsvrc_2012_mean.npy" #"/home/mpss2015/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy"
     )
+
+    load_label_mapping()
 
     # Make sure all frontend assets are compiled
     subprocess.Popen("webpack")
